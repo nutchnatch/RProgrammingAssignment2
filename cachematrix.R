4538f90d56dad3d05a4676cdb169e69dab160b86
nivaldomonteiro <nushnivaldo@gmail.com>
2023-08-13 01:02:02 +0200

    Added first function

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<-y
+    m <- NULL
+  }
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
